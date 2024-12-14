4d3d8291d7bd0d4dbfeaa9773b2e6dfc3061c15c
1Frustrated <rasskazkira@gmail.com>
2024-12-14 12:50:15 +0300

    PZ_3

--- PZ_3/Dop/Dop_3.py
+++ PZ_3/Dop/Dop_3.py
@@ -3,7 +3,10 @@ try:
     if num < 10 or num > 99:
         print("Введи двузначное число")
     else:
-        num = str
-        print(sum(num[0] + num[1]))
+        sm = (num // 10) + (num % 10)
+    if sm % 2 == 0:
+        print(num + 2)
+    else:
+        print(num - 2)
 except ValueError:
-    print("Введите число")
+    print("Введи число")
