8b43b2efbca1a7b9a5c593cf1445f8a75087a187
1Frustrated <rasskazkira@gmail.com>
2024-12-17 22:13:34 +0300

    pz_6

--- PZ_6/test.py
+++ PZ_6/test.py
@@ -1,32 +1,16 @@
 import random
-
-
-def generate_random_list(size):
-    return [random.randint(-100, 100) for _ in range(size) if random.randint(-1, 1) != 0]
-
-
-def check_alternation(numbers):
-    if len(numbers) < 2:
-        return 0
-
-    previous_sign = 1 if numbers[0] > 0 else -1
-
-    for i in range(1, len(numbers)):
-        current_sign = 1 if numbers[i] > 0 else -1
-        if current_sign == previous_sign:
+def check(a):
+    f = None
+    for i in range(len(a)):
+        if f is None:
+            f = a[i] < 0
+        elif (a[i] > 0) == f:
+            f = not f
+        else:
             return i + 1
-        previous_sign = current_sign
     return 0
 
 
-try:
-    N = int(input("Введите размер списка N: "))
-    if N <= 0:
-        print("Размер списка должен быть положительным.")
-    else:
-        numbers = generate_random_list(N)
-        print("Сгенерированный список:", numbers)
-        result = check_alternation(numbers)
-        print(result)
-except ValueError:
-    print("Пожалуйста, введите корректное целое число.")
+a = [random.randint(-10, 10) for _ in range(10)]
+print(a)
+print(check(a))
