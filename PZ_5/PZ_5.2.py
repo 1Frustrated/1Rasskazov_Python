a1dfe0fac0c31efdae8faad989b4c7ca6843413b
1Frustrated <rasskazkira@gmail.com>
2024-12-18 22:59:37 +0300

    pz_5

--- PZ_5/PZ_5.2.py
+++ PZ_5/PZ_5.2.py
@@ -2,28 +2,17 @@
 # положительного числа K на обратный (K — параметр целого типа, являющийся
 # одновременно входным и выходным). С помощью этой функции поменять порядок
 # следования цифр на обратный для каждого из пяти данных целых чисел.
-
-
 def InvertDigits(K):
-    return int(str(K)[::-1])
+    K = str(K)[::-1]
+    return K
 
 
-numbers = []
-print("Введите 5 целых положительных чисел:")
-for i in range(5):
-    while True:
-        try:
-            num = int(input(f"Число {i + 1}: "))
-            if num > 0:
-                numbers.append(num)
-                break
-            else:
-                print("Пожалуйста, введите положительное целое число.")
-        except ValueError:
-            print("Некорректный ввод. Пожалуйста, введите целое число.")
-
-
-inverted_numbers = [InvertDigits(num) for num in numbers]
-
-
-print("Инвертированные числа:", inverted_numbers)
+ListAppend = []
+i = 0
+while i < 5:
+    value = int(input('Введи число списка: '))
+    ListAppend.append(value)
+    i += 1
+for num in ListAppend:
+    inv_num = InvertDigits(num)
+    print(inv_num, end=' ')
